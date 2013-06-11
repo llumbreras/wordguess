267a6949c51b9d5f4ebcd4735baa78dc2e7a083d
L Lumbreras <llumbreras@gmail.com>
2013-06-10 17:39:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordguess::Application.config.secret_token = '29989b1c0d2261eb415a98156f38b097be9780523f6e7dd86bd1bacec0e5c1343815ff0e5d8c87b4865ed55b9fbaa56c2297f2fbb7292a022770586f8fbe9653'
